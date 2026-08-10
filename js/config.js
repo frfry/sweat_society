364e82d301d75bd3d982365bd5087571e0867bee
Kylee T <38272410+frfry@users.noreply.github.com>
2026-08-10 14:37:00 -0600

    Add Annie to allowed users list

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@
 // last4: the last 4 digits of that person's phone number, as a string.
 const ALLOWED_USERS = [
    { firstName: "Kylee", last4: "2911" },
-  // { firstName: "Jamie", last4: "5678" },
+   { firstName: "Annie", last4: "5405" }
 ];
