21deda50a3109bc1208911703406a1fa72ab1a18
Kylee T <38272410+frfry@users.noreply.github.com>
2026-08-10 14:33:43 -0600

    Add Kylee to allowed users
    Added Kylee to the allowed users list.

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // firstName: any case, will be matched case-insensitively.
 // last4: the last 4 digits of that person's phone number, as a string.
 const ALLOWED_USERS = [
-  // { firstName: "Alex", last4: "1234" },
+   { firstName: "Kylee", last4: "2911" },
   // { firstName: "Jamie", last4: "5678" },
 ];
